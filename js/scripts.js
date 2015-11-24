19adca7b41aa2581b91bc328e81b1ee38ecc68a7
Jacob Shwartz <jshwartz@gmail.com>
2015-11-24 11:54:22 -0800

    build three functions to test if divisible by 15, 3, or 5 and then return ping-pong, ping, or pong.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var divisible15 = function(userNumber) {
+  if (userNumber % 15 === 0) {
+    return "ping-pong";
+  }
+};
+
+var divisible3 = function(userNumber) {
+  if (userNumber % 3 === 0) {
+    return "ping";
+  }
+};
+
+var divisible5 = function(userNumber) {
+  if (userNumber % 5 === 0) {
+    return "pong";
+  }
+};
