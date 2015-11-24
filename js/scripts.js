6b45f8c14b255f8ca627d8f99aca14058defb9a5
Jacob Shwartz <jshwartz@gmail.com>
2015-11-24 11:58:19 -0800

    modified scripts.js so that divisiblity functions return true if divisible by 15, 3, or 5.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
 var divisible15 = function(userNumber) {
   if (userNumber % 15 === 0) {
-    return "ping-pong";
+    return true;
+  } else {
+    return false
   }
 };
 
 var divisible3 = function(userNumber) {
   if (userNumber % 3 === 0) {
-    return "ping";
+    return true;
+  } else {
+    return false
   }
 };
 
 var divisible5 = function(userNumber) {
   if (userNumber % 5 === 0) {
-    return "pong";
+    return true;
+  } else {
+    return false
   }
 };
+
+
+$(document).ready(function() {
+
+});
