1b7ce5e6fb308f5565da4e4c2aab9e33e49fce18
Jacob Shwartz <jshwartz@gmail.com>
2015-11-24 11:45:19 -0800

    created specs to test if 3 equals ping, 5 equals pong, and 15 equals ping-pong.

--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,17 @@
-describe('cryptoSquare', function() {
-  it("crptos a perfect square string", function() {
-    expect(cryptoSquare("This is test square.")).to.equal("tisu hsta itsr seqe");
+describe('divisible15', function() {
+  it("returns 'ping-pong' if number divisible by 15", function() {
+    expect(divisibleThree(15)).to.equal("ping-pong");
   });
+});
 
+describe('divisible3', function() {
+  it("returns 'ping' if number divisible by 3", function() {
+    expect(divisibleThree(9)).to.equal("ping");
+  });
+});
 
-
+describe('divisible5', function() {
+  it("returns 'pong' if number divisible by 5", function() {
+    expect(divisibleThree(10)).to.equal("pong");
+  });
 });
