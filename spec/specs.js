c2e8dbcd8fd54625caf6eb86f44cb0125de2a649
Jacob Shwartz <jshwartz@gmail.com>
2015-11-24 11:55:16 -0800

    fix specs to reference proper functions in scripts.js.

--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,17 @@
 describe('divisible15', function() {
   it("returns 'ping-pong' if number divisible by 15", function() {
-    expect(divisibleThree(15)).to.equal("ping-pong");
+    expect(divisible15(15)).to.equal("ping-pong");
   });
 });
 
 describe('divisible3', function() {
   it("returns 'ping' if number divisible by 3", function() {
-    expect(divisibleThree(9)).to.equal("ping");
+    expect(divisible3(9)).to.equal("ping");
   });
 });
 
 describe('divisible5', function() {
   it("returns 'pong' if number divisible by 5", function() {
-    expect(divisibleThree(10)).to.equal("pong");
+    expect(divisible5(10)).to.equal("pong");
   });
 });
