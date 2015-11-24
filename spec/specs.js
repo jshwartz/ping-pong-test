c7a7a46d1ea062fa1e664ca2c4077601ec025ef9
Jacob Shwartz <jshwartz@gmail.com>
2015-11-24 11:36:50 -0800

    loaded spec files, including chai and macha files.

---
+++ spec/specs.js
@@ -0,0 +1,8 @@
+describe('cryptoSquare', function() {
+  it("crptos a perfect square string", function() {
+    expect(cryptoSquare("This is test square.")).to.equal("tisu hsta itsr seqe");
+  });
+
+
+
+});
