502b663d0afb22a195d4a7bec8b76b743e1a1644
Jacob Shwartz <jshwartz@gmail.com>
2015-11-24 12:16:33 -0800

    modify specs to also test for false returns.

--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,30 @@
 describe('divisible15', function() {
-  it("returns 'ping-pong' if number divisible by 15", function() {
+  it("returns 'true' if number divisible by 15", function() {
     expect(divisible15(15)).to.equal(true);
   });
 });
 
+describe('divisible15', function() {
+  it("returns 'false' if number not divisible by 15", function() {
+    expect(divisible15(14)).to.equal(false);
+  });
+});
+
 describe('divisible3', function() {
-  it("returns 'ping' if number divisible by 3", function() {
+  it("returns 'true' if number divisible by 3", function() {
     expect(divisible3(9)).to.equal(true);
   });
 });
 
+describe('divisible3', function() {
+  it("returns 'false' if number not divisible by 3", function() {
+    expect(divisible3(10)).to.equal(false);
+  });
+});
+
 describe('divisible5', function() {
-  it("returns 'pong' if number divisible by 5", function() {
+  it("returns 'true' if number divisible by 5", function() {
     expect(divisible5(10)).to.equal(true);
   });
+
 });
