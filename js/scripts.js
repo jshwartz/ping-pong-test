e42046c4d468ae66b6fb332a78452852a7c36249
Jacob Shwartz <jshwartz@gmail.com>
2015-11-24 12:29:31 -0800

    create 'for' logic to use 'ping' for 3 in list, 'pong' for 5, and 'ping-pong' for 15.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var divisible15 = function(userNumber) {
   if (userNumber % 15 === 0) {
     return true;
   } else {
-    return false
+    return false;
   }
 };
 
@@ -10,7 +10,7 @@ var divisible3 = function(userNumber) {
   if (userNumber % 3 === 0) {
     return true;
   } else {
-    return false
+    return false;
   }
 };
 
@@ -18,7 +18,7 @@ var divisible5 = function(userNumber) {
   if (userNumber % 5 === 0) {
     return true;
   } else {
-    return false
+    return false;
   }
 };
 
@@ -27,8 +27,17 @@ $(document).ready(function() {
 
   var userNumber = parseInt(prompt("Enter a number to ping-pong!"));
 
+
+  debugger;
   for (var i = 1; i <= userNumber; i++) {
-    $("ul#list").append("<li>" + i + "</li>")
+    if (divisible15(i)) {
+      $("ul#list").append("<li>" + "ping-pong" + "</li>");
+    } else if (divisible3(i)) {
+      $("ul#list").append("<li>" + "ping" + "</li>");
+    } else if (divisible5(i)) {
+      $("ul#list").append("<li>" + "pong" + "</li>");
+    } else     
+    $("ul#list").append("<li>" + i + "</li>");
   };
 
 });
