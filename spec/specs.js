df98c3eee8618c5a13f10fc88b025d241721b998
Jacob Shwartz <jshwartz@gmail.com>
2015-11-25 08:27:01 -0800

    modify specs.js so that both true and false tests of divisibility functions are within same 'describe' spec function.

--- spec/specs.js
+++ spec/specs.js
@@ -2,9 +2,6 @@ describe('divisible15', function() {
   it("returns 'true' if number divisible by 15", function() {
     expect(divisible15(15)).to.equal(true);
   });
-});
-
-describe('NOTdivisible15', function() {
   it("returns 'false' if number not divisible by 15", function() {
     expect(divisible15(14)).to.equal(false);
   });
@@ -14,9 +11,6 @@ describe('divisible3', function() {
   it("returns 'true' if number divisible by 3", function() {
     expect(divisible3(9)).to.equal(true);
   });
-});
-
-describe('NOTdivisible3', function() {
   it("returns 'false' if number not divisible by 3", function() {
     expect(divisible3(10)).to.equal(false);
   });
@@ -26,9 +20,6 @@ describe('divisible5', function() {
   it("returns 'true' if number divisible by 5", function() {
     expect(divisible5(10)).to.equal(true);
   });
-});
-
-describe('NOTdivisible5', function() {
   it("returns 'false' if number not divisible by 5", function() {
     expect(divisible5(11)).to.equal(false);
   });
